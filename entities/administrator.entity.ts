5917da2657ea8da09a8ea546553526ebf80e4a3b
Branko Rakonjac <brankorakonjac036@gmail.com>
2020-08-15 01:48:09 +0200

    "prvi"

--- entities/administrator.entity.ts
+++ entities/administrator.entity.ts
@@ -3,8 +3,9 @@ import { Entity, PrimaryGeneratedColumn, Column } from "typeorm";
 @Entity()
 export class Administrator {
     @PrimaryGeneratedColumn({ name: 'administrator_id' , type: 'int', unsigned: true })
-    administratoriId: number;
-
+    administratorId: number;
+    
+    
     @Column({ type: 'varchar', lenght: '32', unique: true})
     username: string;
 
