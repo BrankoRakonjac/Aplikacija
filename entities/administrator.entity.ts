bf0914306b2f42b990954325d82dfffd2fca930b
Branko Rakonjac <brankorakonjac036@gmail.com>
2020-08-15 01:38:08 +0200

    create the administrator table service

---
+++ entities/administrator.entity.ts
@@ -0,0 +1,14 @@
+import { Entity, PrimaryGeneratedColumn, Column } from "typeorm";
+
+@Entity()
+export class Administrator {
+    @PrimaryGeneratedColumn({ name: 'administrator_id' , type: 'int', unsigned: true })
+    administratoriId: number;
+
+    @Column({ type: 'varchar', lenght: '32', unique: true})
+    username: string;
+
+
+    @Column({ name: 'password_hash' ,type: 'varchar' , lenght: '128'})
+    passwordHash: string;
+}
--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,23 @@
 import { Controller, Get } from '@nestjs/common';
+import { Administrator } from 'entities/administrator.entity';
+import { AdministratorService } from './services/administrator/administrator.service';
 
 
 @Controller()
 export class AppController {
+  constructor(
+    private administratorService: AdministratorService
+  ){}
   
   @Get()
   getHello(): string {
     return "hello world";
+  }
+
+  @Get('api/administrator')
+  getAllAdmins(): Promise<Administrator[]>{
+    return this.administratorService.getAll();
+
   }
 
 }
