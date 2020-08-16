285d4ac3af4090db2911d2c12129b7a3bcdeb2f8
Branko Rakonjac <brankorakonjac036@gmail.com>
2020-08-16 02:28:12 +0200

    treca

--- src/controllers/app.controller.ts
+++ src/controllers/app.controller.ts
@@ -1,6 +1,6 @@
 import { Controller, Get } from '@nestjs/common';
 import { Administrator } from 'entities/administrator.entity';
-import { AdministratorService } from './services/administrator/administrator.service';
+import { AdministratorService } from '../services/administrator/administrator.service';
 
 
 
@@ -15,10 +15,5 @@ export class AppController {
     return "hello world";
   }
 
-  @Get('api/administrator')
-  getAllAdmins(): Promise<Administrator[]>{
-    return this.administratorService.getAll();
-
-  }
 
 }
