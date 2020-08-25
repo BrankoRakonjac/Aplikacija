d76eb7e3d3dbe42e4ffbddd5184b30d2bd1e2cdb
Branko Rakonjac <brankorakonjac036@gmail.com>
2020-08-25 02:07:03 +0200

    Add data validation to the application

--- src/dtos/administrator/edit.administrator.dto.ts
+++ src/dtos/administrator/edit.administrator.dto.ts
@@ -1,5 +1,12 @@
+import * as Validator from 'class-validator';
+
+
 export class EditAdministratorDto{
     
+
+    @Validator.IsNotEmpty()
+    @Validator.IsString()
+    @Validator.Length(6, 128)
     password: string;
     
 }
